6c0ec49a7c6d5d532c91d539e4be15c04c4fc9cd
Andrea <andreaortez11@gmail.com>
2024-11-21 00:52:55 -0600

    registro

--- frontend/src/pages/IniciarSesion.tsx
+++ frontend/src/pages/IniciarSesion.tsx
@@ -36,13 +36,23 @@ export default function LogIn() {
 
     return (
         <>
-            <div id="login">
-                <div id="logo">
+            {/* Form */}
+            <div className="LG_SU-background float-start" style={{ paddingTop: '35px' }}>
+                {/* Flecha */}
+                <div className="back position-absolute top-0 end-0">
+                    <Link href="/">
+                        <img src="imagenes/atras.png" alt="Regresar" width="25" height="25" />
+                    </Link>
+                </div>
+                {/* Logo */}
+                <div id="logo" className="position-absolute top-0 start-0 d-flex align-items-center p-2">
                     <img src="./imagenes/logo.png" alt="Logo" width="40" height="40" className="d-inline-block align-text-top" />
                     <span className="navbar-brand mb-0 ms-2">$YUPI</span>
                 </div>
-                <p className="LG_SUtitle">Iniciar Sesión</p>
-                <form onSubmit={handleSubmit} className="center">
+
+
+                <p className="LG_SUtitle text-center" >Iniciar Sesión</p>
+                <form onSubmit={handleSubmit}>
                     <div className="row mb-3">
                         <label htmlFor="inputEmail3" className="col-sm-3 col-form-label">
                             Correo Electrónico
@@ -71,59 +81,61 @@ export default function LogIn() {
                             />
                         </div>
                     </div>
-                    <button className="UserButton btn" type="submit">
-                        Iniciar Sesión
-                    </button>
+                    <div className="text-center">
+                        <button className="UserButton btn" type="submit">
+                            Iniciar Sesión
+                        </button>
+                    </div>
                     <div className="alternativa">
                         <p>¿No tienes una cuenta? </p>
                         <Link href="/Registrarse" className="textColor"> Haz click aquí para registrarte.</Link>
                     </div>
                 </form>
+
             </div >
-            <div className="image">
-                <div className="overlay" id="ovl2">
+            {/* Imagen */}
+            < div className="image position-absolute top-0 end-0" >
+                <div className="overlay ovl2">
                     <p className="title2">Bienvenido a</p>
                     <p className="title">$YUPI</p>
                     <p className="text">Inicia Sesión para acceder a tu cuenta.</p>
                 </div>
                 <img src="imagenes/FondoLI.png" alt="Iniciar Sesión" width="100%" height="630px" className="d-inline-block align-text-top" />
-            </div>
-            <div className="back">
-                <Link href="/">
-                    <img src="imagenes/atras.png" alt="Regresar" width="25" height="25" className="d-inline-block align-text-top" />
-                </Link>
-            </div>
+            </div >
+
 
             {/* Modal */}
-            {showModal && (
-                <div className="modal fade show d-block" tabIndex={-1} style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
-                    <div className="modal-dialog modal-dialog-centered">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">{modalTitle}</h5>
-                                <button
-                                    type="button"
-                                    className="btn-close"
-                                    aria-label="Close"
-                                    onClick={() => setShowModal(false)}
-                                ></button>
-                            </div>
-                            <div className="modal-body">
-                                <p>{modalMessage}</p>
-                            </div>
-                            <div className="modal-footer">
-                                <button
-                                    type="button"
-                                    className="btn btn-secondary UserButton"
-                                    onClick={() => setShowModal(false)}
-                                >
-                                    Cerrar
-                                </button>
+            {
+                showModal && (
+                    <div className="modal fade show d-block" tabIndex={-1} style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+                        <div className="modal-dialog modal-dialog-centered">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title">{modalTitle}</h5>
+                                    <button
+                                        type="button"
+                                        className="btn-close"
+                                        aria-label="Close"
+                                        onClick={() => setShowModal(false)}
+                                    ></button>
+                                </div>
+                                <div className="modal-body">
+                                    <p>{modalMessage}</p>
+                                </div>
+                                <div className="modal-footer">
+                                    <button
+                                        type="button"
+                                        className="btn btn-secondary UserButton"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Cerrar
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            )}
+                )
+            }
         </>
     );
 };
