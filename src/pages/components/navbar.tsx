df99c1371b021ffe0371eec81afc3f960630a379
Andrea <andreaortez11@gmail.com>
2024-11-20 18:04:39 -0600

    start page ready

--- src/pages/components/navbar.tsx
+++ src/pages/components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary static">
         <div className="container-fluid">
           <a className="navbar-brand d-flex align-items-center" href="#">
             <img src="./imagenes/logo.png" alt="Logo" width="40" height="40" className="d-inline-block align-text-top" />
@@ -14,9 +14,9 @@ export default function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <div className="navbar-nav ms-auto">
-              <a className="nav-link" href="#">Sobre Nosotros</a>
-              <a className="nav-link" href="#">Cómo Funciona</a>
-              <a className="nav-link" href="#">Contáctanos</a>
+              <a className="nav-link" href="#sobre-nosotros">Sobre Nosotros</a>
+              <a className="nav-link" href="#como-funciona">Cómo Funciona</a>
+              <a className="nav-link" href="#contactanos">Contáctanos</a>
               <Link href="/IniciarSesion" className="nav-link">Iniciar Sesión</Link>
             </div>
             <button className="btn UserButton" type="button">
