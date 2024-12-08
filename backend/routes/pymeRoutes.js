d52bb84137f76e738cff6c03b16cd66f7ac542c2
Tatiana-Garcia <tzgarcia_16@unitec.edu>
2024-12-08 14:21:30 -0600

    "Mensajes"

--- backend/routes/pymeRoutes.js
+++ backend/routes/pymeRoutes.js
@@ -3,6 +3,6 @@ const { getPyme, getMensajesList } = require('../controllers/pymeController');
 const router = express.Router();
 
 router.get('/Pyme', getPyme);
-router.get('/MensajesP', getMensajesList);
+router.get('/Mensajes', getMensajesList);
 
 module.exports = router;
